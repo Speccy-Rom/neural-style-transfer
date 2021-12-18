e6aa58e412bc5b3d8076c992218059fbe18303b7
speccy-rom <speccy.rom@gmail.com>
2021-12-18 14:08:19 +0300

    update: get_image() func, for create the asynchronous tasks

--- frontend/main.py
+++ frontend/main.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 import streamlit as st
 from PIL import Image
@@ -14,23 +16,37 @@ STYLES = {
     "udnie": "udnie",
 }
 
-# https://discuss.streamlit.io/t/version-0-64-0-deprecation-warning-for-st-file-uploader-decoding/4465
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
-# определяем заголовок h1
 st.title("Style transfer web app")
 
-# отображает виджет загрузчика файлов
 image = st.file_uploader("Choose an image")
 
-# отображает виджет выбора для стилей
 style = st.selectbox("Choose the style", [i for i in STYLES.keys()])
 
-# отображает кнопку
 if st.button("Style Transfer"):
     if image is not None and style is not None:
         files = {"file": image.getvalue()}
         res = requests.post(f"http://backend:8080/{style}", files=files)
         img_path = res.json()
         image = Image.open(img_path.get("name"))
-        st.image(image, width=500)
+        st.image(image)
+
+        displayed_styles = [style]
+        displayed = 1
+        total = len(STYLES)
+
+        st.write("Generating other models...")
+
+        while displayed < total:
+            for style in STYLES:
+                if style not in displayed_styles:
+                    try:
+                        path = f"{img_path.get('name').split('.')[0]}_{STYLES[style]}.jpg"
+                        image = Image.open(path)
+                        st.image(image, width=500)
+                        time.sleep(1)
+                        displayed += 1
+                        displayed_styles.append(style)
+                    except:
+                        pass
