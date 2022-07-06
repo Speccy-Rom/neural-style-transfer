b15e2c59f83e1db408180abdc125765d4216241a
Sourcery AI <>
2022-07-06 19:38:07 +0000

    'Refactored by Sourcery'

--- frontend/main.py
+++ frontend/main.py
@@ -22,31 +22,30 @@ st.title("Style transfer web app")
 
 image = st.file_uploader("Choose an image")
 
-style = st.selectbox("Choose the style", [i for i in STYLES.keys()])
+style = st.selectbox("Choose the style", list(STYLES.keys()))
 
-if st.button("Style Transfer"):
-    if image is not None and style is not None:
-        files = {"file": image.getvalue()}
-        res = requests.post(f"http://backend:8080/{style}", files=files)
-        img_path = res.json()
-        image = Image.open(img_path.get("name"))
-        st.image(image)
+if st.button("Style Transfer") and image is not None and style is not None:
+    files = {"file": image.getvalue()}
+    res = requests.post(f"http://backend:8080/{style}", files=files)
+    img_path = res.json()
+    image = Image.open(img_path.get("name"))
+    st.image(image)
 
-        displayed_styles = [style]
-        displayed = 1
-        total = len(STYLES)
+    displayed_styles = [style]
+    displayed = 1
+    total = len(STYLES)
 
-        st.write("Generating other models...")
+    st.write("Generating other models...")
 
-        while displayed < total:
-            for style in STYLES:
-                if style not in displayed_styles:
-                    try:
-                        path = f"{img_path.get('name').split('.')[0]}_{STYLES[style]}.jpg"
-                        image = Image.open(path)
-                        st.image(image, width=500)
-                        time.sleep(1)
-                        displayed += 1
-                        displayed_styles.append(style)
-                    except:
-                        pass
+    while displayed < total:
+        for style in STYLES:
+            if style not in displayed_styles:
+                try:
+                    path = f"{img_path.get('name').split('.')[0]}_{STYLES[style]}.jpg"
+                    image = Image.open(path)
+                    st.image(image, width=500)
+                    time.sleep(1)
+                    displayed += 1
+                    displayed_styles.append(style)
+                except:
+                    pass
